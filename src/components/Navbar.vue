711350d4b70865649b4d25d103faf09486fb0560
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2022-12-16 10:31:28 -0500

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,66 +1,145 @@
 <template>
 
     <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-----------XXL BREAKPOINT------------>
-            <div class="container d-none d-xxl-block" style="margin-left:650px">
-                <ul class="navbar-nav" id="navbar-wrapper">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">VEHICLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">TIRES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">WHEELS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HARDWARE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CALCULATORS</a>
-                    </li>                                
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">ACCOUNT</a>
-                    </li>                                
-                </ul>
-            </div>
-            <!----XL BREAKPOINT------>
-            <div class="container d-none d-xl-block d-xxl-none" style="margin-left:275px">
-                <ul class="navbar-nav" id="navbar-wrapper">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">VEHICLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">TIRES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">WHEELS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HARDWARE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CALCULATORS</a>
-                    </li>                                
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">ACCOUNT</a>
-                    </li>                                
-                </ul>
-            </div>
-            <!----LG BREAKPOINT------>
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="container d-none d-lg-block d-xl-none" style="margin-left:100px">
+                <!-----------XXL BREAKPOINT------------>
+                <div class="container d-none d-xxl-block" style="margin-left:650px">
+                    <ul class="navbar-nav" id="navbar-wrapper">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">VEHICLE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">TIRES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">WHEELS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HARDWARE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">CALCULATORS</a>
+                        </li>                                
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">ACCOUNT</a>
+                        </li>                                
+                    </ul>
+                </div>
+                <!----XL BREAKPOINT------>
+                <div class="container d-none d-xl-block d-xxl-none" style="margin-left:275px">
+                    <ul class="navbar-nav" id="navbar-wrapper">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">VEHICLE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">TIRES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">WHEELS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HARDWARE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">CALCULATORS</a>
+                        </li>                                
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">ACCOUNT</a>
+                        </li>                                
+                    </ul>
+                </div>
+                <!----LG BREAKPOINT------>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="container d-none d-lg-block d-xl-none" style="margin-left:100px">
+                        <ul class="navbar-nav" id="navbar-wrapper">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">VEHICLE</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">TIRES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">WHEELS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">HARDWARE</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">CALCULATORS</a>
+                            </li>                                
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">ACCOUNT</a>
+                            </li>                                
+                        </ul>
+                    </div>
+                </div>
+                <!----MD BREAKPOINT------>
+                <div class="container d-none d-md-block d-lg-none" style="padding-left:10px">
+                    <ul class="navbar-nav" id="navbar-wrapper">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">VEHICLE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">TIRES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">WHEELS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HARDWARE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">CALCULATORS</a>
+                        </li>                                
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">ACCOUNT</a>
+                        </li>                                
+                    </ul>
+                </div>
+                <!----SM BREAKPOINT------>
+                <div class="container d-none d-sm-block d-md-none" style="padding-left:5px">
+                    <ul class="navbar-nav" id="navbar-wrapper">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">VEHICLE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">TIRES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">WHEELS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">HARDWARE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">CALCULATORS</a>
+                        </li>                                
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">ACCOUNT</a>
+                        </li>                                
+                    </ul>
+                </div>
+                <!----XS BREAKPOINT------>
+                <div class="container d-block d-sm-none" style="padding-left:5px">
                     <ul class="navbar-nav" id="navbar-wrapper">
                         <li class="nav-item">
                             <a href="#" class="nav-link">HOME</a>
@@ -86,87 +165,8 @@
                     </ul>
                 </div>
             </div>
-            <!----MD BREAKPOINT------>
-            <div class="container d-none d-md-block d-lg-none" style="padding-left:10px">
-                <ul class="navbar-nav" id="navbar-wrapper">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">VEHICLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">TIRES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">WHEELS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HARDWARE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CALCULATORS</a>
-                    </li>                                
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">ACCOUNT</a>
-                    </li>                                
-                </ul>
-            </div>
-            <!----SM BREAKPOINT------>
-            <div class="container d-none d-sm-block d-md-none" style="padding-left:5px">
-                <ul class="navbar-nav" id="navbar-wrapper">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">VEHICLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">TIRES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">WHEELS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HARDWARE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CALCULATORS</a>
-                    </li>                                
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">ACCOUNT</a>
-                    </li>                                
-                </ul>
-            </div>
-            <!----XS BREAKPOINT------>
-            <div class="container d-block d-sm-none" style="padding-left:5px">
-                <ul class="navbar-nav" id="navbar-wrapper">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">VEHICLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">TIRES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">WHEELS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HARDWARE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CALCULATORS</a>
-                    </li>                                
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">ACCOUNT</a>
-                    </li>                                
-                </ul>
-            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
 </template>
 
@@ -176,14 +176,6 @@
 
 <style scoped>
 
-@media only screen and (min-width: 992px) {
-    .navbar-nav {
-        height: 40px;
-        font: 'Nunito Sans' ; 
-        font-style: normal;
-        /* font-size: 14px; */
-    }
-}
     .nav-link:hover {
         color: rgba(43, 172, 227, 1);
         font-weight: bold;
@@ -195,13 +187,9 @@
         
     }
     @media only screen and (max-width: 990px) {
-        .nav-link {
-            margin-left: 5px;
-        }
         .nav-item {
             border-bottom: solid;
             border-bottom-color: #CCCCCC;
         }
     }
-
 </style>
